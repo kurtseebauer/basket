d429f11a54283e9400ce4cf6d72ace6a8d7a475e
Kurt Seebauer <kurt.seebauer@gmail.com>
2017-04-18 01:07:54 +0200

    whitespace

--- src/aboutdata.cpp
+++ src/aboutdata.cpp
@@ -25,9 +25,9 @@
 #include <KLocalizedString>
 
 static const char description[] = I18N_NOOP(
-                                      "<p><b>Taking care of your ideas.</b></p>"
-                                      "<p>A note-taking application that makes it easy to record ideas as you think, and quickly find them later. "
-                                      "Organizing your notes has never been so easy.</p>");
+        "<p><b>Taking care of your ideas.</b></p>"
+                "<p>A note-taking application that makes it easy to record ideas as you think, and quickly find them later. "
+                "Organizing your notes has never been so easy.</p>");
 
 // Or how to make order of disorganized toughts.
 
@@ -36,33 +36,20 @@ AboutData::AboutData()
                      VERSION, i18n(description), KAboutLicense::GPL_V2,
                      i18n("Copyright © 2003–2007, Sébastien Laoût; Copyright © 2013–2017, Gleb Baryshev"),
                      QString(),
-                     "http://basket.kde.org/")
-{
+                     "http://basket.kde.org/") {
     //Pass basket.kde.org to constructor to be used as D-Bus domain name, but set the displayed address below
     setHomepage("https://launchpad.net/basket");
     setBugAddress("https://bugs.launchpad.net/basket");
 
-    addAuthor(i18n("OmegaPhil"),
-              i18n("Paste as plaintext option"),
-              "OmegaPhil@startmail.com");
+    addAuthor(i18n("OmegaPhil"), i18n("Paste as plaintext option"), "OmegaPhil@startmail.com");
 
-    addAuthor(i18n("Kelvie Wong"),
-              i18n("Maintainer"),
-              "kelvie@ieee.org");
+    addAuthor(i18n("Kelvie Wong"), i18n("Maintainer"), "kelvie@ieee.org");
 
-    addAuthor(i18n("Sébastien Laoût"),
-              i18n("Original Author"),
-              "slaout@linux62.org");
+    addAuthor(i18n("Sébastien Laoût"), i18n("Original Author"), "slaout@linux62.org");
 
-    addAuthor(i18n("Petri Damstén"),
-              i18n("Basket encryption, Kontact integration, KnowIt importer"),
-              "damu@iki.fi");
+    addAuthor(i18n("Petri Damstén"), i18n("Basket encryption, Kontact integration, KnowIt importer"), "damu@iki.fi");
 
-    addAuthor(i18n("Alex Gontmakher"),
-              i18n("Baskets auto lock, save-status icon, HTML copy/paste, basket name tooltip, drop to basket name"),
-              "gsasha@cs.technion.ac.il");
+    addAuthor(i18n("Alex Gontmakher"), i18n("Baskets auto lock, save-status icon, HTML copy/paste, basket name tooltip, drop to basket name"), "gsasha@cs.technion.ac.il");
 
-    addAuthor(i18n("Marco Martin"),
-              i18n("Original icon"),
-              "m4rt@libero.it");
+    addAuthor(i18n("Marco Martin"), i18n("Original icon"), "m4rt@libero.it");
 }
